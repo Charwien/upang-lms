39f5020f7ee2896d9a04ab2caabb0542264c1421
Melchizedek Joshua Lozano <joshuasuelenlozano.ml@gmail.com>
2024-09-26 21:00:52 +0800

    hi

--- app/src/main/java/com/upang/lms/HomePage.kt
+++ app/src/main/java/com/upang/lms/HomePage.kt
@@ -11,18 +11,8 @@ import com.upang.lms.databinding.ActivityHomePageBinding
 import com.upang.lms.databinding.ActivityLoginPageBinding
 
 class HomePage : AppCompatActivity() {
-    private lateinit var binding: ActivityHomePageBinding
-    private lateinit var firebaseAuth: FirebaseAuth
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityHomePageBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        firebaseAuth = FirebaseAuth.getInstance()
-        
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
